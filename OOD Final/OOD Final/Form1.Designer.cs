dbf9619c95de13705eb849cffb899cbd11e61c8a
JohnMcP96 <johnathan.mcpherson@trojans.dsu.edu>
2025-04-28 19:46:26 -0600

    Hard reset: cleared cached junk and cleaned repo

--- OOD Final/OOD Final/Form1.Designer.cs
+++ OOD Final/OOD Final/Form1.Designer.cs
@@ -28,68 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btnAddIncome = new System.Windows.Forms.Button();
+            this.btnAddExpense = new System.Windows.Forms.Button();
+            this.btnUpdateBudget = new System.Windows.Forms.Button();
+            this.btnViewBudget = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // btnAddIncome
             // 
-            this.button1.Location = new System.Drawing.Point(341, 75);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAddIncome.Location = new System.Drawing.Point(86, 102);
+            this.btnAddIncome.Name = "btnAddIncome";
+            this.btnAddIncome.Size = new System.Drawing.Size(168, 65);
+            this.btnAddIncome.TabIndex = 0;
+            this.btnAddIncome.Text = "🦅Add Income🦅";
+            this.btnAddIncome.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnAddExpense
             // 
-            this.button2.Location = new System.Drawing.Point(480, 75);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnAddExpense.Location = new System.Drawing.Point(441, 102);
+            this.btnAddExpense.Name = "btnAddExpense";
+            this.btnAddExpense.Size = new System.Drawing.Size(168, 65);
+            this.btnAddExpense.TabIndex = 1;
+            this.btnAddExpense.Text = "Add Expense";
+            this.btnAddExpense.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnUpdateBudget
             // 
-            this.button3.Location = new System.Drawing.Point(341, 133);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnUpdateBudget.Location = new System.Drawing.Point(86, 199);
+            this.btnUpdateBudget.Name = "btnUpdateBudget";
+            this.btnUpdateBudget.Size = new System.Drawing.Size(168, 65);
+            this.btnUpdateBudget.TabIndex = 2;
+            this.btnUpdateBudget.Text = "Update Budget";
+            this.btnUpdateBudget.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnViewBudget
             // 
-            this.button4.Location = new System.Drawing.Point(480, 132);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnViewBudget.Location = new System.Drawing.Point(441, 199);
+            this.btnViewBudget.Name = "btnViewBudget";
+            this.btnViewBudget.Size = new System.Drawing.Size(168, 65);
+            this.btnViewBudget.TabIndex = 3;
+            this.btnViewBudget.Text = "View Budget";
+            this.btnViewBudget.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(157, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(382, 38);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "🦅BUDGET🦅CALCULATOR🦅2000🦅";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnViewBudget);
+            this.Controls.Add(this.btnUpdateBudget);
+            this.Controls.Add(this.btnAddExpense);
+            this.Controls.Add(this.btnAddIncome);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnAddIncome;
+        private System.Windows.Forms.Button btnAddExpense;
+        private System.Windows.Forms.Button btnUpdateBudget;
+        private System.Windows.Forms.Button btnViewBudget;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- OOD Final/OOD Final/Form1.cs
+++ OOD Final/OOD Final/Form1.cs
@@ -16,5 +16,10 @@ namespace OOD_Final
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
